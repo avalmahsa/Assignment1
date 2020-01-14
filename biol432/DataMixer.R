af78dc4879f2d4bc3563b3638b76023f13f2af2b
avalmahsa <14ma78@queensu.ca>
2020-01-14 15:02:27 -0500

    pushing initial code for part 2

---
+++ biol432/DataMixer.R
@@ -0,0 +1,22 @@
+MData=read.csv("Data//measurements.csv")
+
+Limb.Area = 1:length(MData$Species)
+UnitsA = 1:length(MData$Species)
+
+for(i in 1:length(MData$Species)){
+  width = MData$Limb.Width[i]
+  length = MData$Limb.Length[i]
+  
+  if(MData$UnitsW[i] == "cm"){
+    width = width*10
+  }
+  
+  if(MData$UnitsL[i] == "cm"){
+    length = length*10
+  }
+  
+  Limb.Area[i] = length * width
+  UnitsA[i] = "mm^2"
+}
+
+MData = as.data.frame(cbind(MData,Limb.Area,UnitsA))
